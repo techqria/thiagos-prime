d369c9911f83347e53af754fb94da5ee5cf2473c
Aenã <aenamartinelli@gmail.com>
2022-10-16 14:09:49 -0300

    inserting_env_vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'http://localhost:3000',
+  awsAccessKeyId: 'AKIATDAWPY26PEZJVTPV',
+  secretAccessKey: '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',
+  region: 'us-east-1',
+  bucket: 'thiagos-prime'
 };
