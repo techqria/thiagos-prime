2b529c38ade9b1173c035adf70aecca5e51c2eb2
Aenã <aenamartinelli@gmail.com>
2022-10-16 14:19:29 -0300

    inserting_env_vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:3000',
+  api: 'https://thiagos-api.herokuapp.com',
   awsAccessKeyId: 'AKIATDAWPY26PEZJVTPV',
   secretAccessKey: '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',
   region: 'us-east-1',
