6a17a839be3a2bb10067cd5ec62834bc6042bd6c
techqria <114258568+techqria@users.noreply.github.com>
2022-12-02 22:39:55 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'https://thiagos-back.vercel.app/',
+  api: 'https://thiagos-back.vercel.app',
   awsAccessKeyId: 'AKIATDAWPY26PEZJVTPV',
   secretAccessKey: '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',
   region: 'us-east-1',
