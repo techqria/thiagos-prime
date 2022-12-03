c82a5eb2997bb8f701928837ac09b787f16e2c69
techqria <114258568+techqria@users.noreply.github.com>
2022-12-02 22:37:00 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  api: 'https://thiagos-api.herokuapp.com',
+  api: 'https://thiagos-back.vercel.app/',
   awsAccessKeyId: 'AKIATDAWPY26PEZJVTPV',
   secretAccessKey: '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',
   region: 'us-east-1',
